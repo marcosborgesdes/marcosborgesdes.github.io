63c2ef691b61aa1413eb961750e8227ade685149
Marcos Borges <sparcos.ds@gmail.com>
2017-03-02 12:27:33 -0300

    atualizacao_fancybox
    fancybox

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-	var $grid = $('.grid-portfolio').isotope({	//inicia o plugin e atribui ele à variavel $grid
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows'
-	});
-	
 	$('.filters-options').on( 'click', 'button', function() {	//atribui todos a todos os botoes dentro do elemento de classe '.filter-options' o evento onclick
 		var filterValue = $(this).attr('data-filter');	//pega o valor do atributo data-filter do botao clicado
 		$grid.isotope({filter: filterValue});	//passa pro plugin o valor que eh pra ser aplicado no filtro(q é o valor q foi pego na linha acima)
 		
 		$('.filters-options button').removeClass('is-checked');	//remove a classe 'is-checked' de todos os botoes do filtro
 		$(this).addClass('is-checked');	//adiciona a classe 'is-checked' somente ao botao q foi clicado
-		
 	});
-	    
-    $grid.isotope({filter: '.htmlcss'});
 	$('.fancybox').fancybox({
             padding : 0,
             openEffect  : 'elastic',
             closeBtn: true,
             fitToView: false
-        });
+      });
 	
 	 $(".scroll").click(function(event){        
         event.preventDefault();
@@ -29,7 +21,16 @@ $(document).ready(function(){
 			 $('html,body').animate({scrollTop:$(this.hash).offset().top});
 		 }
         
-   });
-    
+	});
+	
+	
 });
 
+window.onload = function(){
+	var $grid = $('.grid-portfolio').isotope({	//inicia o plugin e atribui ele à variavel $grid
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows'
+	});	
+	    
+    $grid.isotope({filter: '.htmlcss'});
+}
